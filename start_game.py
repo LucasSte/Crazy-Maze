1ddb68805c4dd77c8cf163bbdd8d28b7068c6228
Ocimar.Acad <ocimar.acad@gmail.com>
2019-03-08 16:00:00 -0300

    Adding Maze Class and a generating function

--- start_game.py
+++ start_game.py
@@ -1,7 +1,15 @@
-#import all libraries here
+# import all libraries here
 
-#import all files here
+# import all files here
 from open_window import *
+from make_maze import *
+
+game_maze = Maze(5, 5)
+print(game_maze.maze)
+
+game_maze.newMaze(10, 10)
+print(game_maze.maze)
+
 
 game_window = Window((1000,667), 'images/initial_background.jpg')
 
