b6d011070114ea96185b8dcf33acb185db8d77ac
Ocimar.Acad <ocimar.acad@gmail.com>
2019-03-17 16:22:47 -0300

    Generation algorithm updated to Prim

---
+++ print_maze.py
@@ -0,0 +1,46 @@
+import pygame
+
+
+
+def print_maze(maze, pxl = 50):
+    pygame.init()
+
+    # Colors:
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    red = (255, 0, 0)
+    green = (0, 255, 0)
+    blue = (0, 0, 255)
+
+    width = maze.num_cells_x * 2 + 1
+    height = maze.num_cells_y * 2 + 1
+    screen_width = width* pxl
+    screen_height = height* pxl
+
+    background = pygame.display.set_mode([screen_width, screen_height])
+
+    sair = False
+
+    while sair == False:
+        background.fill(white)
+
+        for event in pygame.event.get():
+            #print(event)
+            if event.type == pygame.QUIT:
+                sair = True
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    maze.dynamize(1,2)
+
+
+        for y in range(height):
+            for x in range(width):
+                if( maze.maze[y][x] == 1):
+                    pygame.draw.rect(background, black, [x*pxl, y*pxl, pxl, pxl])
+
+
+        pygame.display.update()
+
+    pygame.quit()
+
