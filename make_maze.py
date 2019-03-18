6998319e9fcc2ddfe545e1cc1c76248c8a981191
Ocimar.Acad <ocimar.acad@gmail.com>
2019-03-18 14:33:03 -0300

    Some update

--- make_maze.py
+++ make_maze.py
@@ -1,47 +1,68 @@
-import pygame
-from random import shuffle, randrange
+import numpy as np
+from numpy.random import randint as rand
 
-# Não sei o que faz, entao mantive
-MISSING = object()
+# Usa o algoritmo de Prim, logo o labirinto é um conjunto de celulas conexas
+# (o jogador pode chegar em qualquer celula partindo de qualquer celula)
+
+# IMPORTANTE: Para que o labirinto seja dinamico, o jogador nao deve
+#             ocupar uma poscao que antes era de uma parede, apenas posicao de celulas
 
 
 class Maze:
 
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        pygame.init()
+    def __init__(self, num_cells_x, num_cells_y, startCell=[0, 0]):
+        self.num_cells_x = num_cells_x
+        self.num_cells_y = num_cells_y
+        self.startCell = startCell
         self.maze = self.makeMaze()
 
-    def newMaze(self, newWidth, newHeight):
-        self.__init__(newWidth, newHeight)
+    def dynamize(self, x, y):
+        # [x, y] deverah ser a posição do jogador
+        self.startCell = [x, y]
+        self.maze = self.makeMaze()
 
     def makeMaze(self):
 
-        # casas a serem visitadas:
-        vis = [[0] * self.width + [1] for _ in range(self.height)] + [[1] * (self.width + 1)]
+        width = self.num_cells_x * 2 + 1
+        height = self.num_cells_y * 2 + 1
 
-        # Paredes verticais:
-        ver = [["10"] * self.width + ['1'] for _ in range(self.height)] + [[]]
+        visited = np.zeros([self.num_cells_y, self.num_cells_x], dtype=bool)
+        Maze = np.array([[1] * width, [1, 0] * self.num_cells_x + [1]] * self.num_cells_y + [[1] * width])
 
-        # Paredes horizontais:
-        hor = [["11"] * self.width + ['1'] for _ in range(self.height + 1)]
+        # Mark the cell as visited and add to set
+        visited[self.startCell[1], self.startCell[0] ] = 1
+        path = [self.startCell]
 
-        def walk(x, y):
-            vis[y][x] = 1
+        # While the set of cells is not empty
+        while len(path):
 
-            d = [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]
-            shuffle(d)
-            for (xx, yy) in d:
-                if vis[yy][xx]: continue
-                if xx == x: hor[max(y, yy)][x] = "10"
-                if yy == y: ver[y][max(x, xx)] = "00"
-                walk(xx, yy)
+            # Select randomly a cell to extend the path and remove it from the set
+            [cell_x, cell_y] = path[rand(0, len(path))]
 
-        walk(randrange(self.width), randrange(self.height))
+            # Get available neighbours
+            neighbours = []
+            if cell_x > 0 and not visited[cell_y, cell_x - 1]:
+                neighbours.append([cell_x - 1, cell_y])
 
-        s = ""
-        for (a, b) in zip(hor, ver):
-            s += ''.join(a + ['\n'] + b + ['\n'])
+            if cell_x < self.num_cells_x - 1 and not visited[cell_y, cell_x + 1]:
+                neighbours.append([cell_x + 1, cell_y])
 
-        return s
+            if cell_y > 0 and not visited[cell_y - 1, cell_x]:
+                neighbours.append([cell_x, cell_y - 1])
+
+            if cell_y < self.num_cells_y - 1 and not visited[cell_y + 1, cell_x]:
+                neighbours.append([cell_x, cell_y + 1])
+
+            # Remove the cell if it does not lead anywhere
+            if len(neighbours) == 0:
+                path.remove([cell_x, cell_y])
+
+            else:
+                # Randomly connect to an available cell
+                [cX, cY] = neighbours[rand(0, len(neighbours))]
+                visited[cY, cX] = 1
+                path.append([cX, cY])
+                # Removes the wall between them
+                Maze[(cY + cell_y + 1), (cX + cell_x + 1)] = 0
+
+        return Maze;
