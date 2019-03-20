318d4c4cfa3c1f9dc9e303262e0af4cf21ec7762
Lucas <lucas.tnagel@gmail.com>
2019-03-20 05:08:09 -0400

    Adding a waling figure

--- character.py
+++ character.py
@@ -7,11 +7,13 @@ class Character(pygame.sprite.Sprite, Window):
     def __init__(self, start_position_x, start_position_y):
         pygame.sprite.Sprite.__init__(self)
         self.images = []
-        image_aux = pygame.image.load('images/maze.png')
-        self.images.append(image_aux)
-        self.image = self.images[0]
-        self.rect = self.image.get_rect()
-        self.restrictions = (Window.size[0] - 90, Window.size[1] - 90)
+        self.frame = 0
+        for i in range(1, 4):
+            image_aux = pygame.image.load('images/Walk(' + str(i) + ').png')
+            self.images.append(image_aux)
+            self.image = self.images[0]
+            self.rect = self.image.get_rect()
+        self.restrictions = (Window.size[0] - 120, Window.size[1] - 140)
         self.rect.x = start_position_x
         self.rect.y = start_position_y
 
@@ -19,5 +21,24 @@ class Character(pygame.sprite.Sprite, Window):
         if 0 < x + self.rect.x < self.restrictions[0]:
             self.rect.x += x
 
+            if x > 0:
+                self.frame += 1
+                self.frame = self.frame % 2
+                self.image = self.images[self.frame]
+            elif x < 0:
+                self.frame -= 1
+                if self.frame < 0:
+                    self.frame = 2
+                self.image = self.images[self.frame]
+
         if 0 < y + self.rect.y < self.restrictions[1]:
             self.rect.y += y
+            if y > 0:
+                self.frame += 1
+                self.frame = self.frame % 2
+                self.image = self.images[self.frame]
+            elif y < 0:
+                self.frame -= 1
+                if self.frame < 0:
+                    self.frame = 2
+                self.image = self.images[self.frame]
