09dd10efdeae1084617c5e9c138cee97d0baad15
Lucas <lucas.tnagel@gmail.com>
2019-04-18 06:50:10 -0400

    Finishing end screen

--- open_window.py
+++ open_window.py
@@ -7,6 +7,7 @@ class Action(Enum):
     quit_game = 0
     change_screen = 1
     player_dead = 2
+    local_loop = 3
 
 
 MISSING = object()
@@ -18,28 +19,41 @@ class Window:
     # size = (994, 656)
     size = (1000, 667)
     window = pygame.display.set_mode(size)
-    pressed_start_button = pygame.image.load("images/initial_button_pressed.png")
-    pressed_start_button = pygame.transform.scale(pressed_start_button, (int(720 / 2), int(231 / 2)))
-    start_button = pygame.image.load("images/initial_button.png")
-    start_button = pygame.transform.scale(start_button, (int(720 / 2), int(231 / 2)))
     grass_image = pygame.image.load("images/grass2.jpg")
     grass_image = pygame.transform.scale(grass_image, size)
     heart_image = pygame.image.load("images/heart.png")
     heart_image = pygame.transform.scale(heart_image, (24,24))
+    pressed_start_button = pygame.image.load("images/initial_button_pressed.png")
+    pressed_start_button = pygame.transform.scale(pressed_start_button, (int(720 / 2), int(231 / 2)))
+    start_button = pygame.image.load("images/initial_button.png")
+    start_button = pygame.transform.scale(start_button, (int(720 / 2), int(231 / 2)))
+    exit_button = pygame.image.load("images/exit_button.png")
+    exit_button = pygame.transform.scale(exit_button, (360, 115))
+    pressed_exit_button = pygame.image.load("images/exit_button_pressed.png")
+    pressed_exit_button = pygame.transform.scale(pressed_exit_button, (360, 115))
+    restart_button = pygame.image.load("images/restart_button.png")
+    restart_button = pygame.transform.scale(restart_button, (360, 115))
+    pressed_restart_button = pygame.image.load("images/restart_button_pressed.png")
+    pressed_restart_button = pygame.transform.scale(pressed_restart_button, (360, 115))
 
 
-    def __init__(self, background, maze):
+
+
+    def __init__(self, background, maze_size):
         self.background = background
         pygame.init()
 
         # showMazeScreen variables:
         ## Coloquei elas aqui, pois as imagens precisam ser carregadas apenas uma vez, caso contrario, o jogo fica
         ## muito lento
-        self.pxl_x = self.size[0] / maze.width
-        self.pxl_y = self.size[1] / maze.height
+        self.pxl_x = self.size[0] / (maze_size[0] * 2 + 1)
+        self.pxl_y = self.size[1] / (maze_size[1] * 2 + 1)
 
         self.wall_image = pygame.image.load("images/wall.png")
         self.wall_image = pygame.transform.scale(self.wall_image, (int(self.pxl_x + 1), int(self.pxl_y + 1)))
+
+        self.background_image = pygame.image.load(self.background)
+        self.background_image = pygame.transform.scale(self.background_image, self.size)
 
 
     def showText(self, text, position, font_size, color):
@@ -57,36 +71,9 @@ class Window:
 
     def initialWindow(self):
         pygame.display.set_caption("Crazy Maze")
-        background_image = pygame.image.load(self.background)
-        background_image = pygame.transform.scale(background_image, self.size)
         self.window.blit(self.start_button, (self.size[0] / 2 - 180, 450))
-        self.window.blit(background_image, (0, 0))
+        self.window.blit(self.background_image, (0, 0))
         self.showImage("images/title_initial.png", (2, 40), (int(1996 / 2), int(667 / 2)))
-        action = Action.stand_by
-        while action == Action.stand_by:
-
-            mx, my = pygame.mouse.get_pos()
-
-            for event in pygame.event.get():
-
-                if event.type == pygame.QUIT:
-                    action = Action.quit_game
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    if (self.size[0] / 2 - 180) <= mx <= (self.size[0] / 2 + 180) and 450 <= my <= 555.5:
-                        action = Action.change_screen
-
-            if (self.size[0] / 2 - 180) <= mx <= (self.size[0] / 2 + 180) and 450 <= my <= 555.5:
-                self.window.blit(self.pressed_start_button, (self.size[0] / 2 - 180, 450))
-
-            else:
-                self.window.blit(self.start_button, (self.size[0] / 2 - 180, 450))
-
-            pygame.display.flip()
-
-        return action
-
-    def quitGame(self):
-        pygame.quit()
 
 
     def showMazeScreen(self, player_list, maze, lives):
@@ -104,19 +91,27 @@ class Window:
             self.window.blit(self.heart_image, (110 + 26*i, 3))
         pygame.display.flip()
 
-    def showEndScreen(self):
-        color = (0, 230, 0)
-        self.window.fill(color)
+    def showEndScreen(self, time):
+        self.window.blit(self.background_image, (0, 0))
+        self.showImage("images/text_box.png", (110, 40), (758, 234))
+        self.showText("Você morreu!", (470, 90), 50, (23, 39, 36))
+        self.showText("Você durou apenas", (470, 140), 25, (23, 39, 36))
 
-        action = Action.stand_by
+        if time <= 60:
+            time = int(time*100)
+            time = time/100.0
+            self.showText(str(time) + " segundos no jogo!", (470, 170), 25, (23, 39, 36))
+        else:
+            time = time/60
+            time = int(time*100)
+            time = time/100.0
+            self.showText(str(time) + " minutos no jogo!", (470, 170), 25, (23, 39, 36))
 
-        while action == Action.stand_by:
-            for event in pygame.event.get():
+        self.showText("Tente sair do labirinto", (470, 210), 40, (23, 39, 36))
+        self.showText("sã e salvo!", (470, 250), 40, (23, 39, 36))
+        self.window.blit(self.restart_button, (self.size[0] / 2 - 180, 330))
+        self.window.blit(self.exit_button, (self.size[0] / 2 - 180, 500))
 
-                if event.type == pygame.QUIT:
-                    action = Action.quit_game
-
-            pygame.display.flip()
 
 
 
